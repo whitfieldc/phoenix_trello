d7e655af0b985b2646088cd782e3afb301f90af3
Charles <whitfield.cw@gmail.com>
2016-03-28 23:15:31 -0700

    add Poison JSON encoding to user model

--- web/models/user.ex
+++ web/models/user.ex
@@ -14,6 +14,8 @@ defmodule PhoenixTrello.User do
   @required_fields ~w(first_name last_name email password)
   @optional_fields ~w(encrypted_password)
 
+  @derive {Poison.Encoder, only: [:id, :first_name, :last_name, :email]}
+
   @doc """
   Creates a changeset based on the `model` and `params`.
 
